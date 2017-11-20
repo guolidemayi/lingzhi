0cfa107255fb4e8ea17412459268faeaa3846098
wangwei <guolidemayilang@sina.com>
2017-11-20 10:28:21 +0800

    Initial Commit

---
+++ lingzhi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lingzhi
+//
+//  Created by yiyangkeji on 2017/11/20.
+//  Copyright © 2017年 com.lingzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lingzhi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  lingzhi
+//
+//  Created by yiyangkeji on 2017/11/20.
+//  Copyright © 2017年 com.lingzhi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
