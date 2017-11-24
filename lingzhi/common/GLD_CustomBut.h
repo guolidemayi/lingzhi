2cf7ee6dae04fd328ea103bb625cfcb7b44ee511
wangwei <guolidemayilang@sina.com>
2017-11-24 13:51:46 +0800

    首页文件添加

---
+++ lingzhi/common/GLD_CustomBut.h
@@ -0,0 +1,13 @@
+//
+//  GLD_CustomBut.h
+//  lingzhi
+//
+//  Created by yiyangkeji on 2017/11/24.
+//  Copyright © 2017年 com.lingzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLD_CustomBut : UIButton
+
+@end
---
+++ lingzhi/common/GLD_CustomBut.m
@@ -0,0 +1,21 @@
+//
+//  GLD_CustomBut.m
+//  lingzhi
+//
+//  Created by yiyangkeji on 2017/11/24.
+//  Copyright © 2017年 com.lingzhi. All rights reserved.
+//
+
+#import "GLD_CustomBut.h"
+
+@implementation GLD_CustomBut
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
