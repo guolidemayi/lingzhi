086c1ae18b29b170898838165e60b0f1b4e7767d
wangwei <guolidemayilang@sina.com>
2018-01-10 17:43:15 +0800

    手填城市

--- lingzhi/Classes/Mine/Controller/GLD_ApplyBusnessController.m
+++ lingzhi/Classes/Mine/Controller/GLD_ApplyBusnessController.m
@@ -549,14 +549,14 @@
 - (void)setupAddressTF:(UITableViewCell *)cell {
     if (!_addressTF) {
         _addressTF = [self getTextField:cell];
-        _addressTF.placeholder = @"请选择所在地区";
+        _addressTF.placeholder = @"请填写所在地区";
         __weak typeof(self) weakSelf = self;
-        _addressTF.tapAcitonBlock = ^{
-            //跳转地区
-            [BRStringPickerView showStringPickerWithTitle:@"地区" dataSource:@[@"北京市", @"上海市", @"天津市",@"重庆市",@"河北省",@"山西省",@"台湾省",@"辽宁省",@"吉林省",@"黑龙江省",@"江苏省",@"浙江省",@"安徽省",@"福建省",@"江西省",@"山东省",@"河南省",@"湖北省",@"湖南省",@"广东省",@"甘肃省",@"四川省",@"贵州省",@"海南省",@"云南省",@"青海省",@"陕西省",@"广西壮族自治区",@"西藏自治区",@"宁夏回族自治区",@"新疆维吾尔自治区",@"内蒙古自治区",@"澳门特别行政区",@"香港特别行政区"] defaultSelValue:@"北京市" isAutoSelect:YES resultBlock:^(id selectValue) {
-                weakSelf.addressTF.text = selectValue;
-            }];
-        };
+//        _addressTF.tapAcitonBlock = ^{
+//            //跳转地区
+//            [BRStringPickerView showStringPickerWithTitle:@"地区" dataSource:@[@"北京市", @"上海市", @"天津市",@"重庆市",@"河北省",@"山西省",@"台湾省",@"辽宁省",@"吉林省",@"黑龙江省",@"江苏省",@"浙江省",@"安徽省",@"福建省",@"江西省",@"山东省",@"河南省",@"湖北省",@"湖南省",@"广东省",@"甘肃省",@"四川省",@"贵州省",@"海南省",@"云南省",@"青海省",@"陕西省",@"广西壮族自治区",@"西藏自治区",@"宁夏回族自治区",@"新疆维吾尔自治区",@"内蒙古自治区",@"澳门特别行政区",@"香港特别行政区"] defaultSelValue:@"北京市" isAutoSelect:YES resultBlock:^(id selectValue) {
+//                weakSelf.addressTF.text = selectValue;
+//            }];
+//        };
     }
 }
 
