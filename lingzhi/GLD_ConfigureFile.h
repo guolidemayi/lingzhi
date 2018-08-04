7a177715edb8d5d550ab573ee6cdc9896adde2e2
wangwei <guolidemayilang@sina.com>
2018-08-04 18:40:43 +0800

    接口修改

--- lingzhi/GLD_ConfigureFile.h
+++ lingzhi/GLD_ConfigureFile.h
@@ -11,13 +11,13 @@
 
 //网络请求
 //商品相关
-#define inviteSomeBodyRequest @""//推荐会员收入
+#define inviteSomeBodyRequest @"api/version/getIntegralHistory"//推荐会员收入
 #define storeListRequest @""//商品列表
 #define storeGoodsDetailRequest @""//商品详情
-#define expressRequest @"api/main/banner"//抢单列表
-#define sendExpressRequest @"api/main/banner"//发布快递
+#define expressRequest @"api/version/delivery"//抢单列表
+#define sendExpressRequest @"api/version/addDelivery"//发布快递
 #define robExpressRequest @"api/main/banner"//抢单
-#define sendGoodsRequest @"api/main/banner"//发布商品
+#define sendGoodsRequest @"api/version/addProduct"//发布商品
 #define sendCommentRequest @"api/main/banner"//发布评论
 #define getCommentListRequest @"api/main/banner"//获取评论列表
 #define getShopGoodsListRequest @"api/main/banner"//获取商家商品列表
